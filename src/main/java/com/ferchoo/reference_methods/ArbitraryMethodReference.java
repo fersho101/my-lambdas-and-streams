1ff3942482d518663ef89b824ea20a3a7563f857
fernando corral <146652466+fersho101@users.noreply.github.com>
2024-12-08 17:56:01 -0700

    .

--- src/main/java/com/ferchoo/reference_methods/ArbitraryMethodReference.java
+++ src/main/java/com/ferchoo/reference_methods/ArbitraryMethodReference.java
@@ -6,7 +6,7 @@ public class ArbitraryMethodReference {
     public static void main(String[] args) {
 //        BiPredicate<String, String> eq = (str1, str2) -> str1.equals(str2);
         BiPredicate<String, String> eq = String::equalsIgnoreCase;
-        boolean isEq = eq.test("a", "A");
+        boolean isEq = eq.test("a", "e");
         System.out.println(isEq);
     }
 }
